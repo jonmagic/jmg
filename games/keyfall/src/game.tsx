d5f66efe0306cbca7059f20800b548d3e6f32646
Jonathan Hoyt <jonmagic@gmail.com>
2023-08-16 22:07:05 -0700

    Capture key presses

--- games/keyfall/src/game.tsx
+++ games/keyfall/src/game.tsx
@@ -1,15 +1,29 @@
 import { Canvas } from '@react-three/fiber'
+import { useEffect, useState } from 'react'
 import Key from './key.tsx'
 
-const lowercase = 'abcdefghijklmnopqrstuvwxyz'.split('')
-const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
-const numbers = '0123456789'.split('')
-const all = [...lowercase, ...uppercase, ...numbers]
+// const lowercase = 'abcdefghijklmnopqrstuvwxyz'.split('')
+// const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+// const numbers = '0123456789'.split('')
+// const all = [...lowercase, ...uppercase, ...numbers]
 
 function Game() {
+  const [character, setCharacter] = useState<null | string>(null)
+
+  useEffect(() => {
+    document.addEventListener('keydown', (event) => {
+      const { key } = event
+      setCharacter(key)
+    })
+
+    return () => {
+      document.removeEventListener('keydown', () => {})
+    }
+  })
+
   return (
     <Canvas>
-      <Key character={all[Math.floor(Math.random() * all.length)]} />
+      {character && <Key character={character} />}
     </Canvas>
   )
 }
