80ee649f4c92ccc375476870089b536f47552044
Jonathan Hoyt <jonmagic@gmail.com>
2023-08-16 21:12:34 -0700

    Strip it back to basics

---
+++ games/keyfall/src/keyfall.tsx
@@ -0,0 +1,7 @@
+function Keyfall() {
+  return (
+    <h1>Hello, world!</h1>
+  )
+}
+
+export default Keyfall
--- games/keyfall/src/main.tsx
+++ games/keyfall/src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import Keyfall from './keyfall.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Keyfall />
   </React.StrictMode>,
 )
